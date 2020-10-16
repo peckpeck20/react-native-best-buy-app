f9dd948bf37c762426c09f5b20a8a6d42de55586
Jose Zapata <jose.zapata@columbiaroad.com>
2020-10-16 11:57:01 +0300

    update search bar

--- Components/HomeNavBar.js
+++ Components/HomeNavBar.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import {
-  Row,
-  Icon,
-  Item,
-  Input,
-} from "native-base";
- import { width, height } from "../styles/constant-properties";
-import styles from '../styles/styles';
+import React from "react";
+import { TouchableOpacity } from "react-native";
 
+import { Row, Icon, Input } from "native-base";
+import { width, height } from "../styles/constant-properties";
+import styles from "../styles/styles";
 
-const HomeNavBar = props => {
-  //calculate the width of the search bar
+const HomeNavBar = (props) => {
   const calculatedWidth = Math.min(height, width) * 0.76;
   return (
-
     <Row style={[styles.main, { padding: 5 }]}>
       <Row>
         <Icon
@@ -22,52 +15,35 @@ const HomeNavBar = props => {
           onPress={() => props.handleNav.navigate("DrawerToggle")}
           style={styles.mainIcons}
         />
-        {/* <SearchBar
-                round
-                clearIcon
-                showLoading
-                onChangeText={searchTxt => this.setState({ searchTxt })}
-                onClear={() => this.setState({ searchTxt: "" })}
-                onCancel={() => this.setState({ searchTxt: "" })}
-                containerStyle={{ width: 330, height: 50 }}
-                placeholder="Search Products"
-                icon={{ type: "font-awesome", name: "search" }}
-              /> */}
-        <Item
-          onPress={() =>
-            props.handleNav.navigate("SearchScreen")
-          }
+        <TouchableOpacity
+          onPress={() => props.handleNav.navigate("SearchScreen")}
           rounded
           style={{
             width: calculatedWidth,
             height: 50,
-            padding: 5,
-            backgroundColor: "white"
+            padding: 10,
+            backgroundColor: "white",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 30,
           }}
         >
           <Input placeholder="I'm looking for.." disabled />
           <Icon active name="search" />
-        </Item>
-        <TouchableOpacity onPress={() => props.handleNav.navigate('ShoppingCart')}>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => props.handleNav.navigate("ShoppingCart")}
+        >
           <Icon
             name="shoppingcart"
-            type={'AntDesign'}
+            type={"AntDesign"}
             style={styles.mainIcons}
           />
         </TouchableOpacity>
-
-        {/* <Button
-          onPress={() => props.handleNav.navigate('ShoppingCart')}>
-          <Icon
-            name="ios-cart-outline"
-          />
-        </Button> */}
       </Row>
     </Row>
-
   );
 };
-
-
 
 export default HomeNavBar;
